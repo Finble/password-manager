628a4a4c58d5ea9eaa62c057feb08586c216a631
Finble <elainefinn@hotmail.com>
2016-08-01 17:08:48 +0100

    tested to ensure new accounts can be added and saved locally

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ console.log('starting password manager');
 var storage = require('node-persist');
 storage.initSync();
 
-// test persist works, i.e. that you can save a var/array and get it back (save, then comment out save)
-
-// VARIABLE
-// storage.setItemSync('name', 'Andrew'); // run this then comment out, to test if var saved
-// var name = storage.getItemSync('name');
-// console.log('saved name is: ' + name);
-
-// ARRAY
-// storage.setItemSync('accounts', [{ // test array can be saved too
-//  username: 'Andrew',
-//  balance: 0
-// }]);
-
 var accounts = storage.getItemSync('accounts');
+
+// push on a new account to array - add code, run code, then comment out
+
+// accounts.push({ 
+//     username: 'Jen',
+//     balance: 75
+// });
+
+// save using setItemSync - add code, run code, then comment out - to check object printed out locally (to test all working)
+
+// storage.setItemSync('accounts', accounts);
+
 console.log(accounts);
+
+
+
