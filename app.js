0b984e6eb1d96af362e71ceace9509b65b2c856e
Finble <elainefinn@hotmail.com>
2016-08-01 17:24:59 +0100

    created createAccount and getAccount functions, manually run to test variables saved locally - all working

--- app.js
+++ app.js
@@ -3,20 +3,48 @@ console.log('starting password manager');
 var storage = require('node-persist');
 storage.initSync();
 
-var accounts = storage.getItemSync('accounts');
 
-// push on a new account to array - add code, run code, then comment out
+// create account with name, username + balance
 
-// accounts.push({ 
-//     username: 'Jen',
-//     balance: 75
+function createAccount (account) {
+    var accounts = storage.getItemSync('accounts');
+
+    // ensure we have an empty array to start, if no accounts created before, then value of accounts array will be undefined
+    if (typeof accounts === 'undefined') {
+        accounts = [];
+    }
+    // push on new account to array
+    accounts.push(account);
+    storage.setItemSync('accounts', accounts);  // to accommodate if accounts already exist
+
+    // return account
+    return account;
+}
+
+function getAccount (accountName) {
+    // var accounts, use getItemSync
+    var accounts = storage.getItemSync('accounts');  // saved locally on pc via persist
+    var matchedAccounts;
+
+    accounts.forEach(function (account) {
+        if (account.name === accountName) {
+        matchedAccount = account;
+        }
+    });
+    return matchedAccount;
+}
+
+// DELETE PERSIST FILES FIRST! run createAccount, then comment out
+
+// createAccount({
+//     name: 'Elaine',
+//     username: 'someemail@gmail.com',
+//     password: 'Password123!'
 // });
 
-// save using setItemSync - add code, run code, then comment out - to check object printed out locally (to test all working)
+// run below and terminal should show above object!
 
-// storage.setItemSync('accounts', accounts);
-
-console.log(accounts);
-
+var elaineAccount = getAccount('Elaine');
+console.log(elaineAccount);
 
 
