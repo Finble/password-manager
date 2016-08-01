102d91ae67cd800e796a5df909989656fad264eb
Finble <elainefinn@hotmail.com>
2016-08-01 18:18:48 +0100

    Added argvs module + ability to add user data to terminal - for create, name, username + password - for get, name only.  Can now create and get accounts from terminal

--- app.js
+++ app.js
@@ -3,48 +3,86 @@ console.log('starting password manager');
 var storage = require('node-persist');
 storage.initSync();
 
+// set up argv so that user can input date into terminal, and it will be saved via terminal (so mocking user input)
 
-// create account with name, username + balance
+var argv = require('yargs')
+	.command('create', 'Create a new account', function (yargs) {
+		yargs.options({
+			name: {
+				demand: true,
+				alias: 'n',
+				description: 'Account name (eg: Twitter, Facebook)',
+				type: 'string'
+			},
+			username: {
+				demand: true,
+				alias: 'u',
+				description: 'Account username or email',
+				type: 'string'
+			},
+			password: {
+				demand: true,
+				alias: 'p',
+				description: 'Account password',
+				type: 'string'
+			}
+		}).help('help');
+	})
+	.command('get', 'Get an existing account', function (yargs) {
+		yargs.options({
+			name: {
+				demand: true,
+				alias: 'n',
+				description: 'Account name (eg: Twitter, Facebook)',
+				type: 'string'
+			}
+		}).help('help');
+	})
+	.help('help')
+	.argv;
+var command = argv._[0];
 
 function createAccount (account) {
-    var accounts = storage.getItemSync('accounts');
+	var accounts = storage.getItemSync('accounts');
 
-    // ensure we have an empty array to start, if no accounts created before, then value of accounts array will be undefined
-    if (typeof accounts === 'undefined') {
-        accounts = [];
-    }
-    // push on new account to array
-    accounts.push(account);
-    storage.setItemSync('accounts', accounts);  // to accommodate if accounts already exist
+	if (typeof accounts === 'undefined') {
+		accounts = [];
+	}
 
-    // return account
-    return account;
+	accounts.push(account);
+	storage.setItemSync('accounts', accounts);
+
+	return account;
 }
 
 function getAccount (accountName) {
-    // var accounts, use getItemSync
-    var accounts = storage.getItemSync('accounts');  // saved locally on pc via persist
-    var matchedAccounts;
+	var accounts = storage.getItemSync('accounts');
+	var matchedAccount;
 
-    accounts.forEach(function (account) {
-        if (account.name === accountName) {
-        matchedAccount = account;
-        }
-    });
-    return matchedAccount;
+	accounts.forEach(function (account) {
+		if (account.name === accountName) {
+			matchedAccount = account;
+		}
+	});
+
+	return matchedAccount;
 }
 
-// DELETE PERSIST FILES FIRST! run createAccount, then comment out
+if (command === 'create') {
+	var createdAccount = createAccount({
+		name: argv.name,
+		username: argv.username,
+		password: argv.password
+	});
+	console.log('Account created!');
+	console.log(createdAccount);
+} else if (command === 'get') {
+	var fetchedAccount = getAccount(argv.name);
 
-// createAccount({
-//     name: 'Elaine',
-//     username: 'someemail@gmail.com',
-//     password: 'Password123!'
-// });
-
-// run below and terminal should show above object!
-
-var elaineAccount = getAccount('Elaine');
-console.log(elaineAccount);
-
-
+	if (typeof fetchedAccount === 'undefined') {
+		console.log('Account not found');
+	} else {
+		console.log('Account found!');
+		console.log(fetchedAccount);
+	}
+}
